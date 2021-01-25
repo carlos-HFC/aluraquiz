c0b69a109f3c2b1ef5d9f5babc4b873475aa278e
Carlos <carlos.faustino@bcrcx.com>
2021-01-25 18:15:47 -0300

    Alura quiz, aula 1

---
+++ pages/index.js
@@ -0,0 +1,46 @@
+import styled from 'styled-components'
+
+import { Footer, GitHubCorner, QuizBackground, QuizLogo, Widget } from '../src/components'
+import db from '../db.json'
+
+{/* <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet"></link> */}
+
+const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`
+
+export default function Home() {
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>{db.description}</p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da galera</h1>
+
+            <p>lorem ipsum</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/carlos-HFC" />
+    </QuizBackground>
+  )
+}
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export * from './GitHubCorner'
+export * from './Footer'
+export * from './QuizLogo'
+export * from './QuizBackground'
+export * from './Widget'
